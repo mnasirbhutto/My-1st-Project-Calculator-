678e230685df52769bc06838b1c8a87cadc95ff9
mnasirbhutto <163853245+mnasirbhutto@users.noreply.github.com>
2024-03-25 13:48:56 +0500

    1st commit
    Simple Calculator

---
+++ README.md
@@ -0,0 +1,3 @@
+"# My-1st-Project-Calculator-" 
+"# My-1st-Project-Calculator-" 
+"# My-1st-Project-Calculator-" 
---
+++ index.js
@@ -0,0 +1,32 @@
+#! /usr/bin/env node
+// Shabang
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type: "number", name: "firstNumber" },
+    { message: "Enter second number", type: "number", name: "secondNumber" },
+    {
+        message: "Select one of the operators to perform operation",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Division", "persantage", "no one"],
+    },
+]);
+// condition statement
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstNumber * answer.secondNumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstNumber / answer.secondNumber);
+}
+else if (answer.operator === "persantage") {
+    console.log(answer.firstNumber * 100 / answer.secondNumber);
+}
+else {
+    console.log("please select a valid operator");
+}
